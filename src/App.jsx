298268b59c8c8b7ebbcdcf7a4cbb2fc33a5aa408
zgushonkaK <vsevolod.gor@yandex.ru>
2024-04-13 20:52:05 +0300

    favourite list + receiving user_id

--- src/App.jsx
+++ src/App.jsx
@@ -19,28 +19,34 @@ export class App extends React.Component {
 
   constructor(props) {
     super(props);
-    console.log('constructor');
+    //console.log('constructor');
 
     this.state = {
       text: '',
+      favorites: [],
+      user_id: '',
+      joke_id: '',
+      showFavorites: false,
     }
 
     this.assistant = initializeAssistant(() => this.getStateForAssistant());
 
     this.assistant.on("data", (event/*: any*/) => {
-      console.log(`assistant.on(data)`, event);
+      //console.log(`assistant.on(data)`, event);
       const { action } = event;
-      console.log('action cur = ', action);
+      //console.log('action cur = ', action);
       this.dispatchAssistantAction(action); 
     });
 
     this.assistant.on("start", (event) => {
-
-      let initialData = this.assistant.getInitialData();
-
-      console.log(`assistant.on(start)`, event, initialData);
+      //console.log(`assistant.on(start)`, event);
+      //const { action } = event;
+      //this.dispatchAssistantAction(action);
     });
     
+    this.assistant.on("tts", (event) => {
+      //console.log(`assistant.on(tts)`, event);
+    });
   }
 
   getStateForAssistant() {
@@ -48,6 +54,8 @@ export class App extends React.Component {
     const state = {
       joke: {
         text: this.text,
+        favorites: this.favorites,
+        joke_id: this.joke_id,
       },
     };
     console.log('getStateForAssistant: state:', state)
@@ -55,11 +63,15 @@ export class App extends React.Component {
   }
 
   dispatchAssistantAction(action){
-    console.log('dispatchAssistantAction', action);
+    //console.log('dispatchAssistantAction', action);
     if (action){
       switch (action.type) {
         case 'generate_joke':
-          this.fillTextField(action);
+          this.fillTextField();
+          break;
+        case 'initialize_user':
+          this.setState({user_id: action.id});
+          console.log('user_is', action.id);
           break;
         default:
           throw new Error();
@@ -72,16 +84,63 @@ export class App extends React.Component {
     return content;
   }
 
-  fillTextField(action) {
-    console.log('fillTextField', action)
+  fillTextField() {
     //Мы тут типо как-то получаем анекдот и преобразовываем его в строку newText
     let newText = this.getJokeFromJson();
     this.setState({text: newText});
   }
 
+  addFavorite = () => {
+    const { jokeName } = this.state;
+    if (jokeName.trim() !== '') {
+      this.setState((prevState) => ({
+        favorites: [...prevState.favorites, { id: Date.now(), name: jokeName }],
+      }));
+    }
+  };
+
+  removeFavorite = (id) => {
+    this.setState((prevState) => ({
+      favorites: prevState.favorites.filter((favorite) => favorite.id !== id),
+    }));
+  };
+
+  toggleFavorites = () => {
+    this.setState((prevState) => ({
+      showFavorites: !prevState.showFavorites,
+    }));
+  };
+
+
   render(){
     return (
       <body>
+        <div>
+          <button style={{ position: 'absolute', top: 10, right: 10 }} onClick={this.toggleFavorites}>
+            Избранное
+          </button>
+          {this.state.showFavorites && (
+            <div className="overlay">
+              <div className="favorites-container">
+                <button className="close-button" onClick={this.toggleFavorites}>
+                  &times;
+                </button>
+                <h3>Список избранного:</h3>
+                <ul>
+                  {this.state.favorites.map((favorite) => (
+                    <li key={favorite.id}>
+                      {favorite.name}{' '}
+                      <button onClick={() => this.removeFavorite(favorite.id)}>
+                        Удалить
+                      </button>
+                    </li>
+                ))}
+                </ul>
+                <button onClick={this.addFavorite}>Добавить в избранное</button>
+              </div>
+            </div>
+          )}
+        </div>
         <div className="App">
           <header className="App-header">
             <p className="App-header-text">Tut budet logo</p>
